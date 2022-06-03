6543bc05866082953d41fbb751caca7a407cdb04
samirariva <105059881+samirariva@users.noreply.github.com>
2022-06-03 14:21:24 +0200

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,29 @@
 const submit = document.getElementById("submit")
+const email = document.getElementById("email")
+submit.disabled = true
 
-submit.addEventListener("click", (event) => {
+const validate = () => {
+    if (email.value == "") {
+        submit.disabled = true
+    } else {
+        submit.disabled = false
+    }
+}
+
+email.addEventListener("keyup", (event) => {
+    validate()
+})
+
+
+submit.addEventListener("click", async (event) => {
     event.preventDefault()
-    location.href ="./game.html"
+    const result = await databaseClient.insertInto("users", ["email"], [email.value])
+    if (result.error) {
+        alert("Datenbank Fehler: " + JSON.stringify(result.error, null, 2))
+    }
+    else {
+        // Weiterleitung auf die Game Page  
+        location.href = "./game.html"
+    }
+
 })
